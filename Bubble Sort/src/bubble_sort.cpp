d16f92a3d08da6bca664c6ab2db63863620d07fe
Leviathan <ojoaosoaressantos@gmail.com>
2024-05-19 00:42:58 -0300

    Feat: Some sort algorithms recievied a new way of exchaging itens.

--- Bubble Sort/src/bubble_sort.cpp
+++ Bubble Sort/src/bubble_sort.cpp
@@ -1,24 +1,27 @@
 #include "bubble_sort.hpp"
 
-template<typename T> void bubble_sort(T array[], long long n)
+template<typename T> void bubble_sort(T *array, long long n)
 {
     long long i, j;
-    int nao_trocou;
+    int dont_changed;
 
     for (i = 0; i < n - 1; i++)
     {
-        nao_trocou = 1;
+        dont_changed = 1;
         for (j = 1; j < n - i; j++)
         {
-            if (array[j] < array[j-1]) {
-                troca(array[j], array[j-1]);
-                nao_trocou = 0;
+            if (array[j] < array[j-1])
+            {
+                array[j] = array[j] ^ array[j-1];
+                array[j - 1] = array[j] ^ array[j-1];
+                array[j] = array[j] ^ array[j-1];
+                dont_changed = 0;
             }
         }
 
-        if (nao_trocou) break;
-
+        if (dont_changed) break;
     }
+
     // Análise
     // Buble sort é um método de ordenação ESTÁVEL
     // A complexidade de comparações é O(n²)
@@ -32,6 +35,6 @@ template<typename T> void bubble_sort(T array[], long long n)
         
 }
 
-template void bubble_sort<int>(int array[], long long n);
-template void bubble_sort<long long>(long long array[], long long n);
-template void bubble_sort<double>(double array[], long long n);
+template void bubble_sort<int>(int *array, long long n);
+template void bubble_sort<long long>(long long *array, long long n);
+template void bubble_sort<double>(double *array, long long n);
